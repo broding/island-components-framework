e6637b801035df6931c7c43a0f2600acf51b493b
Bas Roding <bas.roding@gmail.com>
2013-03-19 10:32:37 +0100

    Added SubSystem

---
+++ Island/SubSystem.cpp
@@ -0,0 +1,9 @@
+//
+//  System.cpp
+//  Island
+//
+//  Created by Bas Roding on 19-03-13.
+//  Copyright (c) 2013 Bas Roding. All rights reserved.
+//
+
+#include "System.h"
---
+++ Island/SubSystem.h
@@ -0,0 +1,14 @@
+//
+//  System.h
+//  Island
+//
+//  Created by Bas Roding on 19-03-13.
+//  Copyright (c) 2013 Bas Roding. All rights reserved.
+//
+
+#ifndef __Island__System__
+#define __Island__System__
+
+#include <iostream>
+
+#endif /* defined(__Island__System__) */
