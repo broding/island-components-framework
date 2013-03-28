05cb744df49fc60c273d65fcaf0223cdfefd14b0
Bas Roding <bas.roding@gmail.com>
2013-03-28 21:19:53 +0100

    Added PlayScene

---
+++ Island/src/Scenes/PlayScene.cpp
@@ -0,0 +1,9 @@
+//
+//  PlayScene.cpp
+//  Island
+//
+//  Created by Bas Roding on 28-03-13.
+//  Copyright (c) 2013 Bas Roding. All rights reserved.
+//
+
+#include "PlayScene.h"
---
+++ Island/src/Scenes/PlayScene.h
@@ -0,0 +1,14 @@
+//
+//  PlayScene.h
+//  Island
+//
+//  Created by Bas Roding on 28-03-13.
+//  Copyright (c) 2013 Bas Roding. All rights reserved.
+//
+
+#ifndef __Island__PlayScene__
+#define __Island__PlayScene__
+
+#include <iostream>
+
+#endif /* defined(__Island__PlayScene__) */
