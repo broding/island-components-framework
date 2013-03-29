e0acb1db14cd3650be477be9c03c26c3cae777f8
Bas Roding <bas.roding@gmail.com>
2013-03-29 18:05:01 +0100

    Added CommonFactory

---
+++ Island/src/CommonFactory.cpp
@@ -0,0 +1,9 @@
+//
+//  CommonFactory.cpp
+//  Island
+//
+//  Created by Bas Roding on 29-03-13.
+//  Copyright (c) 2013 Bas Roding. All rights reserved.
+//
+
+#include "CommonFactory.h"
---
+++ Island/src/CommonFactory.h
@@ -0,0 +1,14 @@
+//
+//  CommonFactory.h
+//  Island
+//
+//  Created by Bas Roding on 29-03-13.
+//  Copyright (c) 2013 Bas Roding. All rights reserved.
+//
+
+#ifndef __Island__CommonFactory__
+#define __Island__CommonFactory__
+
+#include <iostream>
+
+#endif /* defined(__Island__CommonFactory__) */
