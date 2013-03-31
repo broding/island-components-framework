3d1a1ca4d34730d11b78419c9581a34256251d71
Bas Roding <bas.roding@gmail.com>
2013-03-31 13:33:36 +0200

    Added StructureFactory

---
+++ Island/src/StructureFactory.h
@@ -0,0 +1,46 @@
+//
+//  StructureFactory.h
+//  Island
+//
+//  Created by Bas Roding on 31-03-13.
+//  Copyright (c) 2013 Bas Roding. All rights reserved.
+//
+
+#ifndef __Island__StructureFactory__
+#define __Island__StructureFactory__
+
+#include <iostream>
+#include "Entity.h"
+#include <SFML/Graphics.hpp>
+#include "RenderComponent.h"
+#include "TransformComponent.h"
+#include "PhysicsComponent.h"
+#include "PlayerInputComponent.h"
+#include "NetworkComponent.h"
+#include "BoxCollisionComponent.h"
+
+class StructureFactory
+{
+public:
+    static Entity* CreateWall(sf::Vector2f position, int length, int width)
+    {
+        Entity* entity = new Entity();
+        TransformComponent* transformComponent = new TransformComponent();
+        transformComponent->position = sf::Vector2f(position);
+        PhysicsComponent* physicsComponent = new PhysicsComponent();
+        NetworkComponent* networkComponent = new NetworkComponent();
+        networkComponent->networkableComponents.push_back(transformComponent);
+        BoxCollisionComponent* collisionComponent = new BoxCollisionComponent();
+        collisionComponent->center = sf::Vector2f(length / 2, width / 2);
+        collisionComponent->size = sf::Vector2f(length, width);
+
+        entity->AddComponent(transformComponent);
+        entity->AddComponent(physicsComponent);
+        entity->AddComponent(networkComponent);
+        entity->AddComponent(collisionComponent);
+        
+        return entity;
+    }
+};
+
+#endif /* defined(__Island__StructureFactory__) */
