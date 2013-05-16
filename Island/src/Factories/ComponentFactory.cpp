3cb7487c990fe8c3cf91f7532cbce3353d8beaad
Bas Roding <bas.roding@gmail.com>
2013-05-16 18:24:56 +0200

    Added ComponentFactory

---
+++ Island/src/Factories/ComponentFactory.cpp
@@ -0,0 +1,14 @@
+//
+//  ComponentFactory.cpp
+//  Island
+//
+//  Created by Bas Roding on 16-05-13.
+//  Copyright (c) 2013 Bas Roding. All rights reserved.
+//
+
+#include "ComponentFactory.h"
+
+Component* ComponentFactory::CreateComponent(pugi::xml_node xml)
+{
+    
+}
