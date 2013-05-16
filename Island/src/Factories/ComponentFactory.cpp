ff0f6eae4b4527ed137f0cf095b96dd99b8247af
Bas Roding <bas.roding@gmail.com>
2013-05-16 18:55:32 +0200

    Components can now be created through their type

--- Island/src/Factories/ComponentFactory.cpp
+++ Island/src/Factories/ComponentFactory.cpp
@@ -8,7 +8,68 @@
 
 #include "ComponentFactory.h"
 
-Component* ComponentFactory::CreateComponent(pugi::xml_node xml)
+#include "RenderComponent.h"
+#include "TransformComponent.h"
+#include "PhysicsComponent.h"
+#include "PlayerInputComponent.h"
+#include "NetworkComponent.h"
+#include "CameraComponent.h"
+#include "BoxCollisionComponent.h"
+#include "SphereCollisionComponent.h"
+#include "HealthComponent.h"
+#include "WeaponComponent.h"
+#include "ScriptComponent.h"
+#include "DamageComponent.h"
+
+Component* ComponentFactory::CreateComponentByType(ComponentType type)
+{
+    switch(type)
+    {
+        case COMPONENT_RENDER:
+            return new RenderComponent();
+            break;
+        case COMPONENT_TRANSFORM:
+            return new TransformComponent();
+            break;
+        case COMPONENT_PHYSICS:
+            return new PhysicsComponent();
+            break;
+        case COMPONENT_PLAYERINPUT:
+            return new PlayerInputComponent();
+            break;
+        case COMPONENT_NETWORK:
+            return new NetworkComponent();
+            break;
+        case COMPONENT_CAMERA:
+            return new CameraComponent();
+            break;
+        case COMPONENT_BOXCOLLISION:
+            return new BoxCollisionComponent();
+            break;
+        case COMPONENT_SPHERECOLLISION:
+            return new SphereCollisionComponent();
+            break;
+        case COMPONENT_HEALTH:
+            return new HealthComponent();
+            break;
+        case COMPONENT_WEAPON:
+            return new WeaponComponent();
+            break;
+        case COMPONENT_SCRIPT:
+            return new ScriptComponent();
+            break;
+        case COMPONENT_DAMAGE:
+            return new DamageComponent();
+            break;
+        case COMPONENT_NULL:
+            return 0;
+            break;
+    }
+    
+    return 0;
+}
+
+Component* ComponentFactory::CreateComponentFromXML(pugi::xml_node xml)
 {
     
 }
